9403c69cc4233aaf2d80bcbe7e255c3942ca5825
AmitKumar1712 <sushantkumat025@gmail.com>
2026-07-16 16:48:27 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/resume_analyser/',
-
-  plugins: [react(),tailwindcss()],
-
+  base: "/resume_analyser/",
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('pdfjs-dist')) return 'pdfjs'
-            return 'vendor'
+          if (id.includes("node_modules")) {
+            if (id.includes("pdfjs-dist")) return "pdfjs";
+            return "vendor";
           }
         },
       },
     },
   },
-})
+});
