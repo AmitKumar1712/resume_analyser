308800844121e6f2d85b6f4c3031bfc8ae7cda7c
AmitKumar1712 <sushantkumat025@gmail.com>
2026-07-16 17:03:46 +0530

    fix config file

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/resume_analyser/",
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("pdfjs-dist")) return "pdfjs";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-});
+})
